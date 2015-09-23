c08f58b02c69ff7ddd7059ee37864b809816c121
Wang Jinghan <phantom.there@gmail.com>
2015-09-23 21:02:16 +0800

    demo cases updated

--- FRHyperLabelDemo/FRHyperLabelDemo/ViewController.m
+++ FRHyperLabelDemo/FRHyperLabelDemo/ViewController.m
@@ -23,15 +23,19 @@
 	FRHyperLabel *label = self.label;
 	label.numberOfLines = 0;
 	
+	//Step 1: Define a normal attributed string for non-link texts
 	NSString *string = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis blandit eros, sit amet vehicula justo. Nam at urna neque. Maecenas ac sem eu sem porta dictum nec vel tellus.";
 	NSDictionary *attributes = @{NSForegroundColorAttributeName: [UIColor blackColor],NSFontAttributeName: [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline]};
-	
 	label.attributedText = [[NSAttributedString alloc]initWithString:string attributes:attributes];
 	
+	//Step 2: Define a selection handler block
 	void(^handler)(FRHyperLabel *label, NSString *substring) = ^(FRHyperLabel *label, NSString *substring){
-		NSLog(@"Selected: %@", substring);
+		UIAlertController *controller = [UIAlertController alertControllerWithTitle:substring message:nil preferredStyle:UIAlertControllerStyleAlert];
+		[controller addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+		[self presentViewController:controller animated:YES completion:nil];
 	};
-	
+
+	//Step 3: Add link substrings
 	[label setLinksForSubstrings:@[@"Lorem", @"Pellentesque", @"blandit", @"Maecenas"] withLinkHandler:handler];
 }
 
