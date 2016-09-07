deade6da13e024539cfe60435e040e5b56dd318c
Jinghan <phantom.there@gmail.com>
2016-09-08 02:21:20 +0800

    take an alternative to calculate the character index

--- FRHyperLabel/FRHyperLabel.m
+++ FRHyperLabel/FRHyperLabel.m
@@ -7,6 +7,7 @@
 //
 
 #import "FRHyperLabel.h"
+#import <CoreText/CoreText.h>
 
 @interface FRHyperLabel ()
 
@@ -14,6 +15,7 @@
 @property (nonatomic) NSLayoutManager *layoutManager;
 @property (nonatomic) NSTextContainer *textContainer;
 @property (nonatomic) NSAttributedString *backupAttributedText;
+@property (nonatomic) CGRect boundingBox;
 
 @end
 
@@ -152,6 +154,58 @@ static UIColor *FRHyperLabelLinkColorHighlight;
 #pragma mark - Substring Locator
 
 - (NSValue *)attributedTextRangeForPoint:(CGPoint)point {
+
+	CGRect boundingBox = [self attributedTextBoundingBox];
+	CGMutablePathRef path = CGPathCreateMutable();
+	CGPathAddRect(path, NULL, boundingBox);
+	
+	CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)self.attributedText);
+	CTFrameRef ctFrame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, self.attributedText.length), path, NULL);
+	
+	
+	CGPoint reversePoint = CGPointMake(point.x, CGRectGetHeight(self.frame) - point.y);
+	
+	CFArrayRef lines = CTFrameGetLines(ctFrame);
+	
+	CGPoint* lineOrigins = malloc(sizeof(CGPoint)*CFArrayGetCount(lines));
+	
+	CTFrameGetLineOrigins(ctFrame, CFRangeMake(0,0), lineOrigins);
+	
+	NSInteger indexOfCharacter = -1;
+	
+	for(CFIndex i = 0; i < CFArrayGetCount(lines); i++) {
+		CTLineRef line = CFArrayGetValueAtIndex(lines, i);
+		
+		CGFloat ascent, descent, leading;
+		CTLineGetTypographicBounds(line, &ascent, &descent, &leading);
+		
+		CGPoint origin = lineOrigins[i];
+		if (reversePoint.y > origin.y - descent) {
+			indexOfCharacter = CTLineGetStringIndexForPosition(line, reversePoint);
+			break;
+		}
+	}
+	
+	free(lineOrigins);
+	CFRelease(ctFrame);
+	CFRelease(path);
+	CFRelease(framesetter);
+	
+	for (NSValue *rangeValue in self.handlerDictionary) {
+		NSRange range = [rangeValue rangeValue];
+		if (NSLocationInRange(indexOfCharacter, range)) {
+			return rangeValue;
+		}
+	}
+
+	return nil;
+}
+
+- (CGRect)attributedTextBoundingBox {
+	if (CGRectGetWidth(_boundingBox) != 0) {
+		return _boundingBox;
+	}
+	
 	NSLayoutManager *layoutManager = [NSLayoutManager new];
 	NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize:CGSizeZero];
 	
@@ -161,26 +215,41 @@ static UIColor *FRHyperLabelLinkColorHighlight;
 	textContainer.size = self.bounds.size;
 	[layoutManager addTextContainer:textContainer];
 	
-	//textStorage to calculate the position
 	NSTextStorage *textStorage = [[NSTextStorage alloc] initWithAttributedString:self.attributedText];
 	[textStorage addLayoutManager:layoutManager];
 	
-	// find the tapped character location and compare it to the specified range
-	CGPoint locationOfTouchInLabel = point;
 	CGRect textBoundingBox = [layoutManager usedRectForTextContainer:textContainer];
-	CGPoint textContainerOffset = CGPointMake((CGRectGetWidth(self.bounds) - CGRectGetWidth(textBoundingBox)) * 0.5 - CGRectGetMinX(textBoundingBox),
-											  (CGRectGetHeight(self.bounds) - CGRectGetHeight(textBoundingBox)) * 0.5 - CGRectGetMinY(textBoundingBox));
-	CGPoint locationOfTouchInTextContainer = CGPointMake(locationOfTouchInLabel.x - textContainerOffset.x, locationOfTouchInLabel.y - textContainerOffset.y);
-	NSInteger indexOfCharacter = [layoutManager characterIndexForPoint:locationOfTouchInTextContainer inTextContainer:textContainer fractionOfDistanceBetweenInsertionPoints:nil];
 	
-	for (NSValue *rangeValue in self.handlerDictionary) {
-		NSRange range = [rangeValue rangeValue];
-		if (NSLocationInRange(indexOfCharacter, range)) {
-			return rangeValue;
-		}
+	
+	CGFloat H = 0;
+	
+	CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString( (CFMutableAttributedStringRef) self.attributedText);
+	CGRect box = CGRectMake(0,0, CGRectGetWidth(textBoundingBox), CGFLOAT_MAX);
+	CFIndex startIndex = 0;
+	CGMutablePathRef path = CGPathCreateMutable();
+	CGPathAddRect(path, NULL, box);
+	CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(startIndex, 0), path, NULL);
+	
+	CFArrayRef lineArray = CTFrameGetLines(frame);
+	CFIndex j = 0, lineCount = CFArrayGetCount(lineArray);
+	CGFloat h, ascent, descent, leading;
+	
+	for (j = 0; j < lineCount; j++) {
+		CTLineRef currentLine = (CTLineRef)CFArrayGetValueAtIndex(lineArray, j);
+		CTLineGetTypographicBounds(currentLine, &ascent, &descent, &leading);
+		h = ascent + descent + leading;
+		H+=h;
 	}
 	
-	return nil;
+	CFRelease(frame);
+	CFRelease(path);
+	CFRelease(framesetter);
+	
+	box.size.height = H;
+	
+	_boundingBox = box;
+	
+	return box;
 }
 
 
